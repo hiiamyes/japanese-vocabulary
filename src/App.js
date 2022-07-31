3ae4343559f358368feb2fdb780d526e16aa48ee
Yes Lee <joshuayes@gmail.com>
2022-07-31 19:55:07 +0800

    wip

--- src/App.js
+++ src/App.js
@@ -1,23 +1,67 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import { random } from "lodash/fp";
+import "./App.css";
+import axios from "axios";
+
+const AIRTABLE_API_BASE_URL = "https://api.airtable.com/v0";
+const TABLE_ID = "tblB1clQaecNCzA5j";
+const APP_ID = "appgZMAf1Cy056FeT";
 
 function App() {
+  const [searchable, setSearchable] = useState(true);
+  const [incorrect, setIncorrect] = useState(false);
+  const [word, setWord] = useState([]);
+  const [words, setWords] = useState([]);
+  useEffect(() => {
+    (async () => {
+      const {
+        data: { records },
+      } = await axios.request({
+        method: "get",
+        url: `${AIRTABLE_API_BASE_URL}/${APP_ID}/${TABLE_ID}`,
+        // params: {
+        // fields: ["id", "chinese", "kanji", "kana"],
+        // sort: [{ field: "id", direction: "asc" }],
+        // },
+        headers: {
+          Authorization: `Bearer ${process.env.REACT_APP_AIRTABLE_TOKEN}`,
+        },
+      });
+      const words = records.map(({ fields }) => fields);
+      setWords(words);
+      console.log(words);
+      setWord(words[random(0, words.length - 1)]);
+      console.log(word);
+      setIncorrect(false);
+    })();
+  }, []);
+  const onSearchCompositionStart = (e) => {
+    setSearchable(false);
+  };
+  const onSearchCompositionEnd = (e) => {
+    setSearchable(true);
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>{word?.chinese}</div>
+      {incorrect && <div>{word?.kanji}</div>}
+      <input
+        onCompositionStart={onSearchCompositionStart}
+        onCompositionEnd={onSearchCompositionEnd}
+        onKeyDown={(e) => {
+          if (searchable) {
+            if (e?.code === "Enter" || e?.keyCode === 13) {
+              if (e.target.value === word.kanji) {
+                setWord(words[random(0, words.length - 1)]);
+                setIncorrect(false);
+                e.target.value = "";
+              } else {
+                setIncorrect(true);
+              }
+            }
+          }
+        }}
+      ></input>
     </div>
   );
 }
